01bf1518776905adc3957f05af49d8be7517f5d6
Monkkham <kkham2021@gmail.com>
2021-06-27 19:06:15 -0700

    scart

---
+++ config/s-cart.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'version'     => '6.2',
+    'sub-version' => '6.2.3',
+    'type'        => 'basic',
+    'homepage'    => 'https://s-cart.org',
+    'title'       => 'Free Open Source eCommerce for Business',
+];
